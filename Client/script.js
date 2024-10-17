152b8ab7f298aefb8890658aedb613d749f0c125
pranam0307 <pranam030798@gmail.com>
2024-10-17 12:58:33 +0530

    Frontend Design Completed & Server Setup Completed

---
+++ Client/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+   loadHTMLTable([]);
+});
+
+function loadHTMLTable(data) {
+    const table = document.querySelector('table tbody');
+    if (data.length === 0){
+        table.innerHTML = "<tr><td class='no-data' colspan='5'  >NO Data</td></tr>";
+    }
+}
